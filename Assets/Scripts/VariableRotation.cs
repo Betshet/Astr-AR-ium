8a28233ce86899546c4124656ea93f54f7a8f4d7
tiffanycoyez-create <tiffany.coyez@gmail.com>
2025-11-07 16:41:28 +0100

    Rotation Gears correction

--- Assets/Scripts/VariableRotation.cs
+++ Assets/Scripts/VariableRotation.cs
@@ -2,17 +2,39 @@ using UnityEngine;
 
 public class RotationGenerique : MonoBehaviour
 {
-    [Header("Paramètres de rotation")]
+    [Header("Parametres de rotation")]
     public float vitesse = 50f;
 
-    [Tooltip("Choisis l'axe de rotation dans l'inspecteur")]
+    // Equivalent de "up" mais modifiable dans l'inspecteur
     public Vector3 axe = Vector3.up;
 
-    [Tooltip("Rotation dans l'espace local ou global")]
+    // Self = repere local, World = repere global
     public Space espace = Space.Self;
+
+    // Pour pouvoir mettre en pause / play
+    public bool rotationActive = true;
 
     void Update()
     {
+        if (!rotationActive)
+            return;
+
         transform.Rotate(axe, vitesse * Time.deltaTime, espace);
+    }
+
+    // Controle depuis un autre script ou un bouton UI
+    public void ActiverRotation()
+    {
+        rotationActive = true;
+    }
+
+    public void MettreEnPauseRotation()
+    {
+        rotationActive = false;
+    }
+
+    public void InverserEtatRotation()
+    {
+        rotationActive = !rotationActive;
     }
 }
